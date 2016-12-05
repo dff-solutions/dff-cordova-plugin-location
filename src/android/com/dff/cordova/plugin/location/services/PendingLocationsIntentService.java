eb5f641a5ad1399b1ed17dbea2316ffe4379d26a
Anthony Nahas <anthony.nahas@dff.solutions.de>
2016-12-05 11:30:48 +0100

    Fix: updated config file of plugin.xml

--- src/android/com/dff/cordova/plugin/location/services/PendingLocationsIntentService.java
+++ src/android/com/dff/cordova/plugin/location/services/PendingLocationsIntentService.java
@@ -2,6 +2,7 @@ package com.dff.cordova.plugin.location.services;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 import com.dff.cordova.plugin.location.resources.LocationResources;
 import com.dff.cordova.plugin.location.utilities.FileHelper;
 
@@ -10,15 +11,19 @@ import com.dff.cordova.plugin.location.utilities.FileHelper;
  */
 public class PendingLocationsIntentService extends IntentService {
 
+    private static final String TAG = "PendingLocationsIntentService";
+
     public PendingLocationsIntentService() {
         super("PendingLocationsIntentService");
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.d(TAG,"onHandleIntent()");
         String action = intent.getAction();
 
         if(action != null){
+            Log.d(TAG,"Action = " + intent.getAction());
             if(action.equals(LocationResources.ACTIONT_INTENT_STORE_PENDING_LOCATIONS)){
                 FileHelper.storePendingLocation(this);
             }
