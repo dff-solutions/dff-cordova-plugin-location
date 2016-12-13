2b44af56b09bba2348f428074d375bf00c6f5ea9
Anthony Nahas <anthony.nahas@dff.solutions.de>
2016-12-13 13:37:59 +0100

    feat: added distance calculator custom holder

---
+++ src/android/com/dff/cordova/plugin/location/utilities/holders/DistanceCalculatorCustomHolder.java
@@ -0,0 +1,48 @@
+package com.dff.cordova.plugin.location.utilities.holders;
+
+import android.location.Location;
+import android.os.Handler;
+import com.dff.cordova.plugin.location.classes.DistanceCalculator;
+import com.dff.cordova.plugin.location.resources.LocationResources;
+
+import java.util.ArrayList;
+
+/**
+ * Created by anahas on 13.12.2016.
+ *
+ * @author Anthony Nahas
+ * @version 1.0
+ * @since 13.12.2016
+ */
+public class DistanceCalculatorCustomHolder implements Runnable {
+
+
+    private static final String TAG = "DistanceCalculatorCustomHolder";
+    private Handler mHandler;
+
+
+    public DistanceCalculatorCustomHolder(Handler mHandler) {
+        this.mHandler = mHandler;
+    }
+
+    @Override
+    public void run() {
+        ArrayList<DistanceCalculator> list = LocationResources.getDistanceCalculatorCustomList();
+        Location lastGoodLocation = LocationResources.getLastGoodLocation();
+
+        if (lastGoodLocation != null) {
+            if (!list.isEmpty()) {
+                int indexOfLastItem = list.size();
+                DistanceCalculator distanceCalculator = new DistanceCalculator(list.get(indexOfLastItem).getEndLocation(),
+                        lastGoodLocation);
+                LocationResources.addDistanceToCustomList(distanceCalculator);
+            } else {
+                //only for the first record
+                DistanceCalculator distanceCalculator = new DistanceCalculator(lastGoodLocation, lastGoodLocation);
+                LocationResources.addDistanceToCustomList(distanceCalculator);
+            }
+        }
+
+        mHandler.postDelayed(this, LocationResources.DISTANCE_CALCULATOR_CUSTOM_DELAY);
+    }
+}
