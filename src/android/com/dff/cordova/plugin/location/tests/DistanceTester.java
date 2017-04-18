b155f3c2a2a75fea7cb76417b1a504f2aa366f87
Anthony Nahas <anthony.na@hotmail.de>
2017-04-18 14:50:08 +0200

    Feat: parse dfstring location to android location

--- src/android/com/dff/cordova/plugin/location/tests/DistanceTester.java
+++ src/android/com/dff/cordova/plugin/location/tests/DistanceTester.java
@@ -1,13 +1,19 @@
 package com.dff.cordova.plugin.location.tests;
 
 import android.content.Context;
+import android.location.Location;
 import android.util.Log;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 /**
@@ -19,6 +25,7 @@ public class DistanceTester {
     public static final String TAG = DistanceTester.class.getSimpleName();
 
     private Context mContext;
+    private ArrayList<Location> locationList;
 
     public DistanceTester(Context context) {
         mContext = context;
@@ -27,6 +34,7 @@ public class DistanceTester {
 
     private void init() {
         Log.d(TAG, "onInit()");
+        locationList = new ArrayList<>();
         readJSON();
     }
 
@@ -46,19 +54,51 @@ public class DistanceTester {
         return json;
     }
 
+
     private void readJSON() {
         JSONObject jsonDATA = null;
+        JSONParser jsonParser = new JSONParser();
         try {
-            jsonDATA = new JSONObject(loadJsonFromAsset());
-        } catch (JSONException e) {
-            Log.e(TAG, "Error: ", e);
-            e.printStackTrace();
+            jsonDATA = (JSONObject) jsonParser.parse(loadJsonFromAsset());
+        } catch (ParseException | ClassCastException e) {
+            System.err.println("Error: " + e);
         }
         if (jsonDATA != null) {
             Log.d(TAG, jsonDATA.toString());
+            readDffStringLocation(jsonDATA);
+
         } else {
             Log.e(TAG, "JSON Array NOT FOUND 404");
         }
+    }
+
+    private void readDffStringLocation(JSONObject jsonObject) {
+        JSONArray dffStringLocationArray = (JSONArray) jsonObject.get("rows");
+        Log.d(TAG, dffStringLocationArray.toString());
+        for (Object DffStringLocationList : dffStringLocationArray) {
+            JSONArray DffStringLocation = (JSONArray) DffStringLocationList;
+            for (Object DffString : DffStringLocation) {
+                locationList.add(parseDffStringLocation(DffString));
+            }
+        }
+        Log.d(TAG, "location list size = " + locationList.size());
+    }
+
+    private static Location parseDffStringLocation(Object dffStringLocation) {
+        String dffString = (String) dffStringLocation;
+        String[] dffString_split = dffString.split(Pattern.quote("|"));
+        Log.d(TAG, dffString);
+        Log.d(TAG, dffString_split[0]);
+        double latitude = (Double.valueOf(dffString_split[0])
+            + (Double.valueOf(dffString_split[1]) / 60)
+            + (Double.valueOf(dffString_split[2]) / 36000));
+        double longitude = (Double.valueOf(dffString_split[3])
+            + (Double.valueOf(dffString_split[4]) / 60)
+            + (Double.valueOf(dffString_split[5]) / 36000));
+        Location location = new Location("GPS");
+        location.setLatitude(latitude);
+        location.setLongitude(longitude);
+        return location;
     }
 
 }
