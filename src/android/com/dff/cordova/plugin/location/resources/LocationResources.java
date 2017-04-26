048ce8cef6e367789baa232a651e9dbc335211b5
Anthony Nahas <anthony.na@hotmail.de>
2017-04-26 14:29:47 +0200

    Ref: renamed mapping action

--- src/android/com/dff/cordova/plugin/location/resources/LocationResources.java
+++ src/android/com/dff/cordova/plugin/location/resources/LocationResources.java
@@ -52,10 +52,9 @@ public class LocationResources {
     public static final String ACTION_SET_LOCATION_LISTENER = "location.action.SET_LOCATION_LISTENER";
     public static final String ACTION_SET_MIN_TIME = "location.action.SET_MIN_TIME";
     public static final String ACTION_CANCEL_STOP_LISTENER = "location.action.CANCEL_STOP_LISTENER";
-    public static final String ACTION_ENABLE_HASHING_LOCATIONS = "location.action.ENABLE_HASHING_LOCATIONS";
+    public static final String ACTION_ENABLE_MAPPING_LOCATIONS = "location.action.ACTION_ENABLE_MAPPING_LOCATIONS";
     public static final String ACTION_SET_STOP_ID = "hash_map.action.SET_STOP_ID";
     public static final String ACTION_CLEAR_STOP_ID = "hash_map.action.CLEAR_STOP_ID";
-
 
 
     //Broadcasts Actions
