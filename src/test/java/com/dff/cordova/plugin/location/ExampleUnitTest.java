239c1188526c2300c9c9c228d9d2953a6034dc68
Anthony Nahas <anthony.na@hotmail.de>
2017-06-06 15:28:39 +0200

    Test: @LocationPluginUnitTest

--- src/test/java/com/dff/cordova/plugin/location/ExampleUnitTest.java
+++ src/test/java/com/dff/cordova/plugin/location/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.dff.cordova.plugin.location;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +12,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
