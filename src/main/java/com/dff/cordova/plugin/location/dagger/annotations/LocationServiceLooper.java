5d5bfe7785ed2b0b986d6a8a9465f22e83d6b2cf
Anthony Nahas <anthony.na@hotmail.de>
2017-06-16 13:35:13 +0200

    Feat: added additional annotations

---
+++ src/main/java/com/dff/cordova/plugin/location/dagger/annotations/LocationServiceLooper.java
@@ -0,0 +1,19 @@
+package com.dff.cordova.plugin.location.dagger.annotations;
+
+import java.lang.annotation.Retention;
+
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Created by anahas on 16.06.2017.
+ *
+ * @author Anthony Nahas
+ * @version 1.0
+ */
+
+@Qualifier
+@Retention(RUNTIME)
+public @interface LocationServiceLooper {
+}
