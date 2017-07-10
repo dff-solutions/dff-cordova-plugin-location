2e2f7c4979b1ad0f3cb089220479f046501d9611
Anthony Nahas <anthony.na@hotmail.de>
2017-07-10 12:12:06 +0200

    Feat: improved res class --> mLastGoodLocation

--- src/main/java/com/dff/cordova/plugin/location/resources/Res.java
+++ src/main/java/com/dff/cordova/plugin/location/resources/Res.java
@@ -1,8 +1,12 @@
 package com.dff.cordova.plugin.location.resources;
 
+import android.location.Location;
+import android.util.Log;
+
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -18,6 +22,9 @@ import javax.inject.Singleton;
 @Singleton
 public class Res {
 
+    public static final String TAG = Res.class.getSimpleName();
+
+    private Location mLastGoodLocation;
     private List<JSONObject> mLocationList;
 
     @Inject
@@ -28,6 +35,7 @@ public class Res {
     /**
      * add synchronized a new location to the location list and return whether the add operation
      * was successful!
+     *
      * @param jsonLocation - a location object as stringified json location
      * @return - whether a location has been added
      */
@@ -36,6 +44,39 @@ public class Res {
             mLocationList.add(jsonLocation);
             return true;
         }
+        Log.d(TAG, "location already exists");
         return false;
+    }
+
+    /**
+     * Clear the last good locations list.
+     */
+    public synchronized void clearDffStringLocationsList() {
+        try {
+            mLocationList.clear();
+        } catch (ConcurrentModificationException e) {
+            Log.e(TAG, "Error while clearing the location list: ", e);
+        }
+    }
+
+    /**
+     * Get the location list - forward synchronized!
+     *
+     * @return - the target location list
+     */
+    public synchronized List<JSONObject> getLocationList() {
+        return mLocationList;
+    }
+
+    public synchronized Location getLastGoodLocation() {
+        return mLastGoodLocation;
+    }
+
+    public synchronized void setLastGoodLocation(Location mLastGoodLocation) {
+        this.mLastGoodLocation = mLastGoodLocation;
+    }
+
+    public synchronized void clearLastGoodLocation() {
+        this.mLastGoodLocation = null;
     }
 }
