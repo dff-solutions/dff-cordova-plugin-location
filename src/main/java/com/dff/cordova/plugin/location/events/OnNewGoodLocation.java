614cc5f8f5156f92fe06af91b3308d6781e51358
Anthony Nahas <anthony.na@hotmail.de>
2017-07-10 11:48:10 +0200

    Feat: @EventBus --> added new event --> OnNewGoodLocation

---
+++ src/main/java/com/dff/cordova/plugin/location/events/OnNewGoodLocation.java
@@ -0,0 +1,20 @@
+package com.dff.cordova.plugin.location.events;
+
+import android.location.Location;
+
+/**
+ * Created by anahas on 07.07.2017.
+ *
+ * @author Anthony Nahas
+ * @version 1.0
+ * @since 07.07.2017
+ */
+
+public class OnNewGoodLocation {
+
+   public Location location;
+
+    public OnNewGoodLocation(Location location) {
+        this.location = location;
+    }
+}
