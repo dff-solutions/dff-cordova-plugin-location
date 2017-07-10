3ff3e35719c2b55d3b035304247f91719e4c293d
Anthony Nahas <anthony.na@hotmail.de>
2017-07-10 12:36:28 +0200

    Feat. improved location helper class --> to kmh method

--- src/main/java/com/dff/cordova/plugin/location/utilities/helpers/LocationHelper.java
+++ src/main/java/com/dff/cordova/plugin/location/utilities/helpers/LocationHelper.java
@@ -3,6 +3,7 @@ package com.dff.cordova.plugin.location.utilities.helpers;
 import android.location.Location;
 import android.util.Log;
 
+import com.dff.cordova.plugin.location.resources.Res;
 import com.google.gson.Gson;
 
 import org.json.JSONException;
@@ -34,5 +35,19 @@ public class LocationHelper {
             Log.e(TAG, "Error: ", e);
         }
         return new JSONObject();
+    }
+
+    /**
+     * Get the speed of the last good saved location.
+     * If it's available, the speed will be converted to KM/h
+     *
+     * @return - The speed of the last good location.
+     */
+    public double toKmh(Location location) {
+        assert location != null;
+        if (location.hasSpeed() && location.getSpeed() > 0) {
+            return Math.round(location.getSpeed() * 3.6);
+        }
+        return 0;
     }
 }
