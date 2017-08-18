e36504c4d94ceef20914b7297f88d63b1693cfff
Anthony Nahas <anthony.na@hotmail.de>
2017-08-18 14:10:56 +0200

    Fix: @get location list --> json array

--- src/main/java/com/dff/cordova/plugin/location/actions/GetLocationListAction.java
+++ src/main/java/com/dff/cordova/plugin/location/actions/GetLocationListAction.java
@@ -3,15 +3,18 @@ package com.dff.cordova.plugin.location.actions;
 import android.util.Log;
 
 import com.dff.cordova.plugin.common.action.Action;
+import com.dff.cordova.plugin.location.classes.GLocation;
 import com.dff.cordova.plugin.location.dagger.annotations.Shared;
 import com.dff.cordova.plugin.location.resources.Res;
 import com.dff.cordova.plugin.location.resources.Resources;
 
+import com.google.gson.Gson;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Get the all pending location that are stored in the location list.
@@ -45,7 +48,17 @@ public class GetLocationListAction extends Action {
             Log.e(TAG, "Error: ", e);
         }
         if (mRes.getLocationList().size() > 0) {
-            callbackContext.success(new JSONArray(mRes.getLocationList()));
+            List<GLocation> clonedList = mRes.getLocationList();
+            Gson gson = new Gson();
+            try {
+                if (clonedList != null) {
+                    callbackContext.success(new JSONArray(gson.toJson(clonedList)));
+                }
+            } catch (JSONException e) {
+                Log.e(TAG, "Error: ", e);
+                callbackContext.error("JSONException + " + e);
+            }
+
             Log.d(TAG, "list > 0 ");
             if (canReset) {
                 mRes.clearList();
