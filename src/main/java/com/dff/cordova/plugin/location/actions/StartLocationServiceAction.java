6a35b9d15439a5e3a0c7424633a3fb660590b14e
Anthony Nahas <anthony.na@hotmail.de>
2018-06-26 12:07:30 +0200

    feat: added logger library

--- src/main/java/com/dff/cordova/plugin/location/actions/StartLocationServiceAction.java
+++ src/main/java/com/dff/cordova/plugin/location/actions/StartLocationServiceAction.java
@@ -3,6 +3,7 @@ package com.dff.cordova.plugin.location.actions;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
 import com.dff.cordova.plugin.dagger2.abstracts.Action;
 import com.dff.cordova.plugin.dagger2.annotations.ApplicationContext;
 import com.dff.cordova.plugin.dagger2.annotations.Shared;
@@ -11,6 +12,8 @@ import com.dff.cordova.plugin.location.events.OnStartLocationService;
 import com.dff.cordova.plugin.location.resources.Resources;
 import com.dff.cordova.plugin.location.services.LocationService;
 import com.dff.cordova.plugin.location.utilities.helpers.PreferencesHelper;
+import com.orhanobut.logger.Logger;
+
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -37,10 +40,10 @@ public class StartLocationServiceAction extends Action {
 
     @Inject
     public StartLocationServiceAction(
-        @ApplicationContext Context mContext,
-        @Shared GLocationManager mGLocationManager,
-        EventBus mEventBus,
-        PreferencesHelper mPreferencesHelper
+            @ApplicationContext Context mContext,
+            @Shared GLocationManager mGLocationManager,
+            EventBus mEventBus,
+            PreferencesHelper mPreferencesHelper
     ) {
         this.mContext = mContext;
         this.mGLocationManager = mGLocationManager;
@@ -64,7 +67,7 @@ public class StartLocationServiceAction extends Action {
                 mContext.startService(new Intent(mContext, LocationService.class));
             }
         } catch (JSONException e) {
-            Log.e(TAG, "Error: ", e);
+            Logger.t(TAG).e(TAG, "Error: " + e.getMessage());
             getCallbackContext().error("Error while trying to start the location service: " + e);
 
         }
@@ -85,7 +88,7 @@ public class StartLocationServiceAction extends Action {
                 getCallbackContext().success();
             } else {
                 getCallbackContext().error("Location Manager is not listening since the service could not be " +
-                    "started or No provider has been found to request a new location");
+                        "started or No provider has been found to request a new location");
             }
         }
     }
