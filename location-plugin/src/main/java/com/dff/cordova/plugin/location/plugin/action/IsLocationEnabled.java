77c86023912e516d7f063a57ea08ecb9155f6ea3
Sebastian Knabe <sknabe@dff-solutions.local>
2020-04-28 10:24:46 +0200

    feat(IsLocationEnabled): add missing action

--- location-plugin/src/main/java/com/dff/cordova/plugin/location/plugin/action/IsLocationEnabled.java
+++ location-plugin/src/main/java/com/dff/cordova/plugin/location/plugin/action/IsLocationEnabled.java
@@ -1,5 +1,10 @@
 package com.dff.cordova.plugin.location.plugin.action;
 
+import android.location.LocationManager;
+import android.os.Build;
+
+import com.dff.cordova.plugin.shared.helpers.AndroidSdkHelper;
+
 import javax.inject.Inject;
 
 /**
@@ -9,16 +14,32 @@ import javax.inject.Inject;
 public class IsLocationEnabled extends LocationAction {
     public static final String ACTION = "isLocationEnabled";
     
+    private AndroidSdkHelper androidSdkHelper;
+    private LocationManager locationManager;
+    
     @Inject
     public IsLocationEnabled(
-   
+        AndroidSdkHelper androidSdkHelper,
+        LocationManager locationManager
     ) {
-    
+        this.androidSdkHelper = androidSdkHelper;
+        this.locationManager = locationManager;
     }
     
     @Override
     protected void execute() {
-    
+        int neededSDK = Build.VERSION_CODES.P;
+        
+        if (androidSdkHelper.isSdkGreaterOrEqual(neededSDK)) {
+            boolean enabled = locationManager.isLocationEnabled();
+            callbackContext.success(enabled ? 1 : 0);
+        } else {
+            callbackContext.error(String.format(
+                "action requires SDK %s or higher. Current SDK: %s",
+                neededSDK,
+                androidSdkHelper.getSdkVersionCode()
+            ));
+        }
     }
     
     @Override
